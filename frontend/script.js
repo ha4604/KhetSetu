dcbdcb6dc7c5832ccb1dcc4cb2aff33e26d45ac2
Harshit Shakya <kushwahaharshit350@gmail.com>
2026-05-30 22:09:54 +0530

    Updated full KhetSetu website

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,36 @@
-alert("Welcome to KhetSetu");
+function cropAdded(){
+    alert("Crop Added Successfully");
+}
+
+function orderPlaced(){
+    alert("Order Placed Successfully");
+}
+
+function searchProducts(){
+
+    let input = document
+    .getElementById("searchInput")
+    .value
+    .toLowerCase();
+
+    let cards = document
+    .getElementsByClassName("product-card");
+
+    for(let i = 0; i < cards.length; i++){
+
+        let productName = cards[i]
+        .getElementsByTagName("h2")[0]
+        .innerText
+        .toLowerCase();
+
+        if(productName.includes(input)){
+            cards[i].style.display = "block";
+        }
+
+        else{
+            cards[i].style.display = "none";
+        }
+
+    }
+
+}
